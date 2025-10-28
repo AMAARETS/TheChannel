6c8ee3e79ba73bd5c1c3c649ca3b560f7decc51f
AMAARETS <DG4107983@GMAIL.COM>
2025-10-28 12:21:22 +0200

    שינוי זמני

--- backend/main.go
+++ backend/main.go
@@ -54,6 +54,8 @@ func cspFrameAncestorsMiddleware(next http.Handler) http.Handler {
 func validateOrigin(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		origin := r.Header.Get("Origin")
+		next.ServeHTTP(w, r)
+		return
 		if len(settingConfig.AllowedOrigins) <= 0 {
 			next.ServeHTTP(w, r)
 			return
